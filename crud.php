e5abfea15b694b2a6132b565877fe6264f25662b
happyzach <schneider61938@gmail.com>
2018-11-19 02:49:35 -0600

    completely separated create function from model

--- crud.php
+++ crud.php
@@ -1,9 +1,11 @@
 <?php
 include_once '../config/db_handler.php';
 class Crud {
+  //Connection and table variables
   private $conn;
   private $table_name;
 
+  //constructor to setup crud object
   public function __construct($db, $obj){
     $this->conn = $db;
     $this->table_name = $obj->table_name;
@@ -12,20 +14,23 @@ class Crud {
   function create($obj, $params){
     //Silliness to convert array to workable strings to pass in sql query
     $params_1 = implode(',', $params);
+    //prepends colon to each array item
     $params_2 = explode(",", (":".implode(",:", $params)));
-    $params_2 = implode(",", $params_2);
+    //creates string for query with colon
+    $params_3 = implode(",", $params_2);
     //insert query
     $query = "INSERT INTO " . $this->table_name . "
-    ($params_1) VALUES($params_2)";
+    ($params_1) VALUES($params_3)";
     //prepare query
     $stmt = $this->conn->prepare($query);
-    //sanatize data
-    $obj->title=htmlspecialchars(strip_tags($obj->title));
-    $obj->body=htmlspecialchars(strip_tags($obj->body));
-    //bind values
-    $stmt-> bindParam(":title", $obj->title);
-    $stmt-> bindParam(":body", $obj->body);
-    //check if it can run
+    //sanatize data and bind VALUES
+    foreach($params as $param){
+      $obj->$param=htmlspecialchars(strip_tags($obj->$param));
+      //prepends colon to param for pdo binding
+      $bind_param = ":".$param;
+      $stmt-> bindParam($bind_param, $obj->$param);
+    }
+    //check if it can run return true if it can
     if($stmt->execute()){
       return true;
     }
