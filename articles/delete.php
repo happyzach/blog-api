7d08f2395a06be8ee6d886ab184466f2362b5fe3
happyzach <schneider61938@gmail.com>
2018-12-04 00:33:15 -0600

    added login check to create update delete articles

--- articles/delete.php
+++ articles/delete.php
@@ -6,44 +6,55 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-// include database and object files
-include_once '../config/db_handler.php';
-include_once '../models/article.php';
+include_once '../session_handler.php';
 
-// instantiate database and article object
-$database = new DBhandler();
-$db = $database->connect();
+//pull in session handler
+$sess = new SessionsHandler();
 
-// initialize object
-$article = new Article();
-$crud = new Crud($db, $article);
-
-//get data
-$content = file_get_contents("php://input");
-$data = json_decode($content);
-
-//make sure data is not empty
-if(!empty($data->id)){
-  $article->id = $data->id;
-
-  //delete record
-  if($crud->delete($article)){
-    //response code 201
-    http_response_code(201);
-    //alert user
-    echo json_encode(array("message" => "Article was deleted"));
-  }
-
-  //tell user that it was not deleted
-  else{
-
-    //response code 503
-    http_response_code(503);
-    echo json_encode(array("message" => "Unable to delete article"));
-  }
+if(!$sess->logged_in()){
+  //if they are already logged in
+  echo json_encode(array('message' => 'Must log in first to do this'));
 }
 else{
-  //data is incomplete
-  http_response_code(400);
-  echo json_encode(array("message" => "Unable to delete article. Something is missing"));
+  // include database and object files
+  include_once '../config/db_handler.php';
+  include_once '../models/article.php';
+
+  // instantiate database and article object
+  $database = new DBhandler();
+  $db = $database->connect();
+
+  // initialize object
+  $article = new Article();
+  $crud = new Crud($db, $article);
+
+  //get data
+  $content = file_get_contents("php://input");
+  $data = json_decode($content);
+
+  //make sure data is not empty
+  if(!empty($data->id)){
+    $article->id = $data->id;
+
+    //delete record
+    if($crud->delete($article)){
+      //response code 201
+      http_response_code(201);
+      //alert user
+      echo json_encode(array("message" => "Article was deleted"));
+    }
+
+    //tell user that it was not deleted
+    else{
+
+      //response code 503
+      http_response_code(503);
+      echo json_encode(array("message" => "Unable to delete article"));
+    }
+  }
+  else{
+    //data is incomplete
+    http_response_code(400);
+    echo json_encode(array("message" => "Unable to delete article. Something is missing"));
+  }
 }
